8b81c3247580eb6d7b7a79d23a475716d1410495
Bora <borabalos9@gmail.com>
2024-05-16 11:38:37 +0200

    Safe area + styling first component

--- react-native-intro/app/(tabs)/index.tsx
+++ react-native-intro/app/(tabs)/index.tsx
@@ -1,33 +1,22 @@
 import Profile from '@/components/Profile';
 import { StyleSheet, Text } from 'react-native';
-
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 
 export default function HomeScreen() {
 	return (
-		<>
-			<Text>Hello World</Text>
-			<Text>Hello You</Text>
-			<Profile />
-		</>
+		<SafeAreaProvider>
+			<SafeAreaView>
+				<Text style={styles.title}>Hello World</Text>
+			</SafeAreaView>
+		</SafeAreaProvider>
 	);
 }
 
 const styles = StyleSheet.create({
-	titleContainer: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		gap: 8,
-	},
-	stepContainer: {
-		gap: 8,
-		marginBottom: 8,
-	},
-	reactLogo: {
-		height: 178,
-		width: 290,
-		bottom: 0,
-		left: 0,
-		position: 'absolute',
+	title: {
+		color: "red",
+		fontSize: 24,
+		marginLeft: 16
 	},
 });
