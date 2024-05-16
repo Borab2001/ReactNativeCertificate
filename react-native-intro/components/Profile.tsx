a1ec765122dc7fb18f36ee5dc8a252f57580cd84
Bora <borabalos9@gmail.com>
2024-05-16 11:20:42 +0200

    First component

---
+++ react-native-intro/components/Profile.tsx
@@ -0,0 +1,12 @@
+import { Text, TouchableOpacity } from "react-native";
+
+const Profile = () => {
+    return <>
+        <Text>Hello World!</Text>
+        <TouchableOpacity>
+            <Text>Click me!</Text>
+        </TouchableOpacity>
+    </>;
+}
+ 
+export default Profile;
