1ab0404e74f72322b817fa3d4b8a8313990fba3a
Bora <borabalos9@gmail.com>
2024-05-16 12:24:32 +0200

    Flexbox styling

--- react-native-intro/app/(tabs)/index.tsx
+++ react-native-intro/app/(tabs)/index.tsx
@@ -6,16 +6,24 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 export default function HomeScreen() {
 	return (
 		<SafeAreaProvider>
-			<SafeAreaView>
+			<SafeAreaView style={styles.section}>
 				<Text style={styles.title}>Hello World</Text>
 				<Text style={styles.subtitle}>Welcome back mate</Text>
-				<View style={styles.box} />
+				<View style={styles.container}>
+					<View style={styles.box} />
+					<View style={styles.box} />
+					<View style={styles.box} />
+				</View>
 			</SafeAreaView>
 		</SafeAreaProvider>
 	);
 }
 
 const styles = StyleSheet.create({
+	section: {
+		backgroundColor: "#fff",
+		flex: 1
+	},
 	title: {
 		color: "black",
 		fontWeight: "500",
@@ -28,14 +36,20 @@ const styles = StyleSheet.create({
 		fontWeight: "500",
 		fontSize: 16,
 		marginHorizontal: 16,
-		marginBottom: 24
+	},
+	container: {
+		flex: 1,
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		gap: 12,
+		marginHorizontal: 16,
+		marginVertical: 24,
 	},
 	box: {
-		width: "auto",
+		width: 48,
 		height: 48,
-		backgroundColor: "#e0e0e0",
-		marginHorizontal: 16,
-		borderRadius: 6
-		
+		backgroundColor: "#f0f0f0",
+		borderRadius: 6,
 	}
 });
