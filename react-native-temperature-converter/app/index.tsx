d148475e2f7f05a09757d70d91a2327dbeb32e55
Bora <borabalos9@gmail.com>
2024-05-29 16:51:47 +0200

    Displaying input value state

--- react-native-temperature-converter/app/index.tsx
+++ react-native-temperature-converter/app/index.tsx
@@ -2,16 +2,20 @@ import { ImageBackground, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "./styles";
 import Input from "@/components/input/Input";
+import { useState } from "react";
 
 
 export default function Index() {
+
+	const [inputValue, setInputValue] = useState("0");
+
   return (
 	<ImageBackground style={styles.backgroundImage} source={require('@/assets/images/hot.png')}>
 		<SafeAreaProvider>
 			<SafeAreaView style={styles.root}>
 				<View style={styles.workspace}>
-					<Text>Temperature</Text>
-					<Input defaultValue="0" />
+					<Text>{inputValue.toString()}</Text>
+					<Input onChange={setInputValue} defaultValue="0" />
 					<Text>Button</Text>
 				</View>
 			</SafeAreaView>
--- react-native-temperature-converter/components/input/Input.tsx
+++ react-native-temperature-converter/components/input/Input.tsx
@@ -3,10 +3,12 @@ import { styles } from "./Input.style";
 
 interface InputProps {
     defaultValue: string;
+    onChange: (text: string) => void;
 }
 
 const Input: React.FC<InputProps> = ({
-    defaultValue
+    defaultValue,
+    onChange
 }) => {    return (
         <View style={styles.container}>
             <TextInput 
@@ -14,6 +16,7 @@ const Input: React.FC<InputProps> = ({
                 maxLength={3} 
                 placeholder="Type your temperature"
                 defaultValue={defaultValue}
+                onChangeText={(text) => onChange(text)}
             />
             <Text style={styles.unit}>°C</Text>
         </View>
